6e07510648733c0cda6a427036af1dafd6a48fa4
young <young.mwy@gmail.com>
2014-05-13 00:07:09 +0800

    output results to result file

--- xb.py
+++ xb.py
@@ -3,39 +3,50 @@
 
 import re
 import string
+import time
 
-# read html file
+DEBUG = 0
+
+# open & read source html file
 html = open('./html')
 html_context = html.read()
 
-# full
+# open target result file
+result = open('./result', 'a+')
+
+# full_rate
 r_full = re.compile(r">满班率<.*?>(\d{1,3}\.\d{2})%<.*?<!--原老师续报率计算-->", re.S)
 full_rate = re.findall(r_full, html_context)
-#print full_rate
+if DEBUG == 1 :
+	print "full_rate :\n" + str(full_rate)
 
-# quit
+# quit_rate
 r_quit = re.compile(r">开课后退费率<.*?>(\d{1,3}\.\d{2})%<.*?<!--学而思续报率计算-->", re.S)
 quit_rate = re.findall(r_quit, html_context)
-#print quit_rate
-
-# spring
+if DEBUG == 1 :
+	print "quit_rate :\n" + str(quit_rate)
+	
+# spring_rate
 r_spring = re.compile(r"春续暑续报率.*?>\(学而思\)<.*?>(\d{1,3}\.\d{2})%<", re.S)
 spring_rate = re.findall(r_spring, html_context)
-#print spring_rate
+if DEBUG == 1 :
+	print "spring_rate :\n" + str(spring_rate)
 
-# autumn
+# autumn_rate
 r_autumn = re.compile(r">\(学而思\)<.*?春续秋续报率.*?>\(学而思\)<.*?>(\d{1,3}\.\d{2})%<", re.S)
 autumn_rate = re.findall(r_autumn, html_context)
-#print autumn_rate
+if DEBUG == 1 :
+	print "autumn rate :\n" + str(autumn_rate)
 
-# class num
+# get class number
 class_num = len(full_rate)
 
-# get average value
+# get four average values
 sum_full = 0.0
 sum_quit = 0.0
 sum_spring = 0.0
 sum_autumn = 0.0
+
 for class_i in range(class_num) :
 	sum_full = sum_full + string.atof(full_rate[class_i])
 	sum_quit = sum_quit + string.atof(quit_rate[class_i])
@@ -47,13 +58,31 @@ ave_quit = sum_quit / class_num / 100
 ave_spring = sum_spring / class_num / 100
 ave_autumn = sum_autumn / class_num / 100
 
-print "满班率 : %0.2f" % (ave_full * 100) + "%"
-print "退费率 : %0.2f" % (ave_quit * 100) + "%"
-print "春季续报率 : %0.2f" % (ave_spring * 100) + "%"
-print "秋季续报率 : %0.2f" % (ave_autumn * 100) + "%"
-
+# calculate final score and level
 score = ((ave_spring + ave_autumn) / 2 * 1.6  + ave_full * 0.4 - ave_quit * 2.0) * 50
-print "总分 : " + str(score)
 
-# close html file
+if score >= 90 :
+	level = 'S'
+elif score >= 85 :
+	level = 'A'
+elif score >= 80 :
+	level = 'B'
+else : 
+	level = 'NOT defined'
+
+# output results to result file
+local_time = time.localtime(time.time())
+current_time = time.strftime('%Y-%m-%d %H:%M:%S\n', local_time) 
+result.writelines(current_time)
+result.writelines("--------------------\n")
+result.writelines("满班率\t: %0.2f" % (ave_full * 100) + " %\n")
+result.writelines("退费率\t: %0.2f" % (ave_quit * 100) + " %\n")
+result.writelines("春续报\t: %0.2f" % (ave_spring * 100) + " %\n")
+result.writelines("秋续报\t: %0.2f" % (ave_autumn * 100) + " %\n")
+result.writelines("--------------------\n")
+result.writelines("总分\t: %0.2f" % (score) + "\n")
+result.writelines("等级\t: " + level + "\n\n\n")
+
+# close source html file & result file
 html.close();
+result.close();
